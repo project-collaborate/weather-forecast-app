6c3d95a9628248e3b5880b155d23adbce37238b1
PeretzNiro <peretz.niro@gmail.com>
2023-06-16 12:13:10 +0100

    Day 3 Pactch
    fix the overridden file

--- Geo.py
+++ Geo.py
@@ -4,8 +4,8 @@ from Framework import Framework
 class Geo(Framework):
     def __init__(self):
         super().__init__()
-        self.result_prob = 0
-        self.user_request = input("Choose a location.\n")
+        self._result_prob = 0
+        self._user_request = input("Choose a location.\n")
         self._url = f"http://api.openweathermap.org/geo/1.0/direct?q={self.user_request}&limit=5&appid={self.api_key}"
         self.coord = self.coordinates(self.request())
 
