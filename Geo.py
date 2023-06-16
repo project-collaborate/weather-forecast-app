338110683fe575f26c4c3701e10dceae1b17ac92
Fmurgioni94 <fr.murgioni94@gmail.com>
2023-06-16 11:41:09 +0100

    day3/finish encapsulation
    few changes on the class Geo

--- Geo.py
+++ Geo.py
@@ -4,14 +4,22 @@ from Framework import Framework
 class Geo(Framework):
     def __init__(self):
         super().__init__()
-        self.result_prob = 0
-        self.user_request = input("Choose a location.\n")
+        self._result_prob = 0
+        self._user_request = input("Choose a location.\n")
         self._url = f"http://api.openweathermap.org/geo/1.0/direct?q={self.user_request}&limit=5&appid=715c3bcddc3796ecec3432f8e4554454"
         self.coord = self.coordinates(self.request())
 
     @property
+    def result_prob(self):
+        return self._result_prob
+    
+    @property
+    def user_request(self):
+        return self._user_request
+    
+    @property
     def url(self):
         return self._url
-
+    
     def coordinates(self, data):
         return [data[self.result_prob].get('lat'), data[self.result_prob].get('lon')]
