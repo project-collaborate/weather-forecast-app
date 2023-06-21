a57db9897da127ab7864d54188da908e118f7784
Fmurgioni94 <fr.murgioni94@gmail.com>
2023-06-21 01:32:37 +0100

    GUI update
    Minor changes to the NewGUI class

--- GUi.py
+++ GUi.py
@@ -12,29 +12,46 @@ class NewGUI:
         self.window.geometry('600x500+200+200')
         self.window.title('This is just a try')
 
+        # ----------------------------- Widgets
+
         self.page_title_label = ctk.CTkLabel(
             self.window,
             text="Weather Forecast App",
-            font=("Arial", 25))
-        self.page_title_label.pack(padx=20, pady=20)
+            font=("Arial", 25)
+        )
+        
 
         self.check_weather_button = ctk.CTkButton(
             self.window,
             text="Check weather", 
-            command=self.button_click_event)
-        self.check_weather_button.pack(padx=20, pady=20)
+            command=self.button_click_event
+        )
+        
+        self.check_weather_frame = ctk.CTkScrollableFrame(
+            master=self.window,
+            width=1300,
+            height=250,
+            border_width=2
+        )
 
         self.dress_recommendation_button = ctk.CTkButton(
             self.window,
             text="Dress Recommendation",
             command=self.dress_recommendation_event)
-        self.dress_recommendation_button.pack()
 
         self.history_button = ctk.CTkButton(
             self.window, 
             text="Dress Recommendation", 
-            command=self.check_history_event)
-        self.history_button.pack(padx=20, pady=20)
+            command=self.check_history_event
+        )
+
+  #     ------------------------ Positions
+
+        self.page_title_label.pack(padx=20, pady=20)
+        self.check_weather_button.pack(padx=20, pady=20)
+        self.check_weather_frame.pack(padx=5, pady=5)
+        self.dress_recommendation_button.pack(padx=10, pady=10)
+        self.history_button.pack(padx=10, pady=10)
 
         self.window.mainloop()
 
@@ -48,28 +65,35 @@ class NewGUI:
         self.req4 = FourDaysForecast(self.req1.coord)
 
         self.label_req1 = ctk.CTkLabel(
-            self.window,
-            text=f"the coordinates of the city are: {self.req1.coord}")
-        self.label_req1.pack()
+            self.check_weather_frame,
+            text=f"the coordinates of the city are: {self.req1.coord}"
+        )
 
         self.label_req2 = ctk.CTkLabel(
-            self.window,
-            text=f"The current weather is: {self.req2.return_data().get('main')}, {self.req2.return_data().get('description')}")
-        self.label_req2.pack()
+            self.check_weather_frame,
+            text=f"The current weather is: {self.req2.return_data().get('main')}, {self.req2.return_data().get('description')}"
+        )
 
         self.label_req3 = ctk.CTkLabel(
-            self.window,
-            text=f"The level of pollution is: {self.req3.return_data()}")
-        self.label_req3.pack()
+            self.check_weather_frame,
+            text=f"The level of pollution is: {self.req3.return_data()}"
+        )
+        
+        self.divider = ctk.CTkLabel(
+            self.check_weather_frame,
+            text='---------------------------------------------------'
+        )
 
         # print(self.req4.return_data())
+
+        self.label_req1.pack(padx= 10, pady= 10)
+        self.label_req2.pack()
+        self.label_req3.pack()
+        self.divider.pack(padx= 10, pady= 10)
+
 
     def dress_recommendation_event(self):
         print("dress recommendation button")
 
     def check_history_event(self):
         print("Check history button")
-
-
-
-# prova1 = NewGUI()
